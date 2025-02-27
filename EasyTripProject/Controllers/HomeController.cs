cadfb62bb467331a3114cd27ff3b4d3996b20dce
senolerdemm <senolerdemerdem1@gmail.com>
2025-02-27 05:07:10 +0300

    home Page development

--- EasyTripProject/Controllers/HomeController.cs
+++ EasyTripProject/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using EasyTripProject.Models;
+using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace EasyTripProject.Controllers;
 
@@ -8,14 +11,33 @@ namespace EasyTripProject.Controllers;
 public class HomeController : Controller
 {
     private readonly ILogger<HomeController> _logger;
-    public HomeController(ILogger<HomeController> logger)
+    private readonly Context _context;  
+    public HomeController(ILogger<HomeController> logger , Context context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
     {
-        return View();
+        var blogs = _context.FreeTravelGuides?.ToList() ?? new List<FreeTravelGuides>();
+        return View(blogs);
+    }
+    public PartialViewResult PartialView1 ()
+    {
+        var recentPosts = _context.FreeTravelGuides?
+            .OrderByDescending(x => x.Date)
+            .Take(3)
+            .ToList();
+        return PartialView(recentPosts);
+    }
+    public PartialViewResult PartialView2 ()
+    {
+        var mostPopularPosts = _context.FreeTravelGuides?
+            .OrderByDescending(x => x.Comments.Count)
+            .Take(3)
+            .ToList();
+        return PartialView(mostPopularPosts);
     }
 
     public IActionResult Privacy()
--- EasyTripProject/Models/Home.cs
+++ EasyTripProject/Models/Home.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace EasyTripProject.Models
 {
     public class Home
     {
+        
         [Key]
         public int Id { get; set; }
         public string ?Header{ get; set; }
